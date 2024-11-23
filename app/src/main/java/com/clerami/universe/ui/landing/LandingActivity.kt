2d5f57a24ca700165f07664035c31d7d71538a18
Clerami <ImClerami@gmail.com>
2024-11-23 10:18:19 +0700

    Landing,Login,Register 1.1

--- app/src/main/java/com/clerami/universe/ui/landing/LandingActivity.kt
+++ app/src/main/java/com/clerami/universe/ui/landing/LandingActivity.kt
@@ -1,5 +1,6 @@
 package com.clerami.universe.ui.landing
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -7,16 +8,36 @@ import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.clerami.universe.R
+import com.clerami.universe.databinding.ActivityLandingBinding
+import com.clerami.universe.ui.login.LoginActivity
+import com.clerami.universe.ui.register.RegisterActivity
 
 class LandingActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLandingBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_landing)
+
+
+        binding = ActivityLandingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.registerButton.setOnClickListener{
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        binding.loginButton.setOnClickListener{
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
         }
 
 
